f2ee2b957499b76a894315238306a91474e821fe
Jason0705 <6jasonli6@gmail.com>
2018-06-16 22:56:05 -0700

    Registration
    Implemented registration with firebase to create user

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -20,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
+        let myDatabase = Database.database().reference()
+        
+        myDatabase.setValue("We've got data!")
+        
         return true
     }
 
