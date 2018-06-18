e491a8712d453c8989fbb50373f1b97921facc8b
Jason0705 <6jasonli6@gmail.com>
2018-06-17 21:00:40 -0700

    Bug fix: data wipe, Update UI
    Fixing the bug of data getting wiped. Updated UI.

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -20,10 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
-        let myDatabase = Database.database().reference()
-        
-        myDatabase.setValue("We've got data!")
-        
         return true
     }
 
